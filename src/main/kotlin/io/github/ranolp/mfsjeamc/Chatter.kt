4e391d26dfad3770c540551c4f617a9d9f60a373
Ranol <asdhugh1@gmail.com>
2018-05-14 23:57:22 +0900

    Update Chatter.kt

--- src/main/kotlin/io/github/ranolp/mfsjeamc/Chatter.kt
+++ src/main/kotlin/io/github/ranolp/mfsjeamc/Chatter.kt
@@ -3,8 +3,8 @@ package io.github.ranolp.mfsjeamc
 import io.github.ranolp.mfsjea.ConversionResult
 import io.github.ranolp.mfsjea.Mfsjea
 import io.github.ranolp.mfsjea.escaper.BracketEscaper
-import io.github.ranolp.mfsjea.keyboard.InputKeyboard
-import io.github.ranolp.mfsjea.keyboard.OutputKeyboard
+import io.github.ranolp.mfsjea.keyboard.*
+import io.github.ranolp.mfsjeamc.dao.ChatterDAO
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import java.util.*
@@ -13,15 +13,49 @@ class Chatter private constructor(private val uuid: UUID) {
     companion object {
         private val CACHE = mutableMapOf<UUID, Chatter>()
 
+        private val inputKeyboards = listOf(
+            QwertyKeyboard,
+            DvorakKeyboard,
+            ColemakKeyboard
+        )
+        private val outputKeyboards = listOf(
+            DubeolStandardKeyboard,
+            Sebeol390Keyboard,
+            SebeolFinalKeyboard
+        )
+
         @JvmName("of")
         operator fun invoke(uuid: UUID) = CACHE.getOrPut(uuid) {
-            Chatter(
-                uuid
-            )
+            Chatter(uuid)
         }
 
         @JvmName("of")
         operator fun invoke(player: Player) = invoke(player.uniqueId)
+    }
+
+    init {
+        val data = ChatterDAO.get(uuid)
+        data["useMfsjea"]?.takeIf { it !is Boolean }?.let {
+            useMfsjea = it as Boolean
+        }
+        data["specified-input"]?.takeIf { it !is String }?.let {
+            val name = it as String
+            val keyboard = inputKeyboards.firstOrNull { it.name == name }
+            if (keyboard == null) {
+                Bukkit.getLogger().severe("입력 키보드 ${name}를 찾을 수 없습니다.")
+            } else {
+                inputKeyboard = keyboard
+            }
+        }
+        data["specified-output"]?.takeIf { it !is String }?.let {
+            val name = it as String
+            val keyboard = outputKeyboards.firstOrNull { it.name == name }
+            if (keyboard == null) {
+                Bukkit.getLogger().severe("출력 키보드 ${name}를 찾을 수 없습니다.")
+            } else {
+                outputKeyboard = keyboard
+            }
+        }
     }
 
     @get:JvmName("useMfsjea")
@@ -66,8 +100,15 @@ class Chatter private constructor(private val uuid: UUID) {
     }
 
     fun save() {
-
+        ChatterDAO.update(this)
+        ChatterDAO.save()
     }
+
+    fun serialize(): Map<String, Any?> = mapOf(
+        "use-mfsjea" to useMfsjea,
+        "specified-input" to inputKeyboard?.name,
+        "specified-output" to outputKeyboard?.name
+    )
 
     fun jeamfs(sentence: String): ConversionResult = mfsjea.jeamfsAuto(sentence)
 }
